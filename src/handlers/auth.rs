f8d141dae72d1e54d36f7db778f31b45ea4ef418
svartalf <self@svartalf.info>
2018-05-30 21:11:45 +0300

    /bearer and /hidden-basic-auth endpoints

--- src/handlers/auth.rs
+++ src/handlers/auth.rs
@@ -1,5 +1,5 @@
-use actix_web::{HttpRequest, HttpResponse, Path, http};
-use actix_web_httpauth::basic::{BasicAuth, Config};
+use actix_web::{HttpRequest, HttpResponse, Path, Result, FromRequest, http};
+use actix_web_httpauth::extractors::{AuthenticationError, basic, bearer};
 
 use application::State;
 
@@ -9,20 +9,48 @@ pub struct Credentials {
     passwd: String,
 }
 
-pub fn valid_auth(path: &Path<Credentials>, auth: &BasicAuth) -> bool {
-    path.user == auth.username && path.passwd == auth.password
-}
-
 /// Prompts the user for authorization using HTTP Basic Auth.
-pub fn basic_auth((req, path, auth): (HttpRequest<State>, Path<Credentials>, BasicAuth)) -> HttpResponse {
-    if !valid_auth(&path, &auth) {
-        BasicAuth::error_response(&Config::default())
-    } else {
+pub fn basic_auth((req, path, auth): (HttpRequest<State>, Path<Credentials>, basic::BasicAuth)) -> Result<HttpResponse> {
+    if path.user == auth.username() && Some(path.passwd.as_str()) == auth.password() {
         let body = json!({
             "authenticated": true,
-            "user": path.user,
+            "user": auth.username(),
         });
-        req.build_response(http::StatusCode::OK)
-            .body(body.to_string())
+        let resp = req.build_response(http::StatusCode::OK)
+            .body(body.to_string());
+        Ok(resp)
+    } else {
+        Err(AuthenticationError::from(basic::Config::default()).into())
     }
+}
+
+/// Prompts the user for authorization using HTTP Basic Auth,
+/// but returns `HTTP 404` instead of `HTTP 401`
+pub fn hidden_basic_auth((req, path): (HttpRequest<State>, Path<Credentials>)) -> HttpResponse {
+    if let Ok(auth) = basic::BasicAuth::extract(&req) {
+        if path.user == auth.username() && Some(path.passwd.as_str()) == auth.password() {
+            let body = json!({
+                "authenticated": true,
+                "user": auth.username(),
+            });
+
+            return req.build_response(http::StatusCode::OK)
+                .body(body.to_string());
+        }
+    }
+
+    req.build_response(http::StatusCode::NOT_FOUND)
+        .finish()
+
+}
+
+/// Checks for HTTP Bearer auth
+pub fn bearer_auth((req, auth): (HttpRequest<State>, bearer::BearerAuth)) -> HttpResponse {
+    let body = json!({
+        "authenticated": true,
+        "token": auth.token(),
+    });
+
+    req.build_response(http::StatusCode::OK)
+        .body(body.to_string())
 }
